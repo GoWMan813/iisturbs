a9af79338bb38767ac471b772c45f33732efcc69
GoWMan813 <gowman@gowman.tv>
2024-03-12 22:05:08 -0500

    Initial upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,208 @@
-import Image from "next/image";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="relative flex place-items-center">
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: "50%",
+            transform: "translateX(-50%)",
+            fontSize: "50px",
+          }}
+        >
+          <RoughNotationGroup show={true}>
+            <RoughNotation
+              type="strike-through"
+              color="black"
+              animationDelay={3000}
+              animationDuration={1500}
+              iterations={1}
+              strokeWidth={10}
+            >
+              <RoughNotation
+                type="crossed-off"
+                color="red"
+                animationDelay={1000}
+                animationDuration={1500}
+                iterations={1}
+              >
+                YES
+              </RoughNotation>
+            </RoughNotation>
+          </RoughNotationGroup>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          style={{
+            position: "absolute",
+            top: 100,
+            left: "50%",
+            transform: "translateX(-50%)",
+            fontSize: "50px",
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <RoughNotationGroup show={true}>
+            <RoughNotation
+              type="highlight"
+              color="yellow"
+              animationDelay={8000}
+              animationDuration={1500}
+              iterations={1}
+            >
+              NO
+            </RoughNotation>
+          </RoughNotationGroup>
+        </div>
+        <RoughNotationGroup show={true}>
+          <div
+            style={{
+              position: "relative",
+              top: 300,
+              left: "50%",
+              transform: "translateX(-50%)",
+              fontSize: "24px",
+            }}
+          >
+            Updating&nbsp;
+            <RoughNotation type="underline" color="black" iterations={1} animationDelay={10000}>
+              anything
+            </RoughNotation>
+            &nbsp;right before you stream is a&nbsp;
+            <RoughNotation type="circle" color="red" iterations={3} animationDelay={11000}>
+              <RoughNotation type="underline" color="black" iterations={1} animationDelay={10500}>
+                very bad idea!
+              </RoughNotation>
+            </RoughNotation>
+          </div>
+        </RoughNotationGroup>
+        <RoughNotationGroup show={true}>
+          <div
+            style={{
+              position: "relative",
+              top: 400,
+              left: "5%",
+              transform: "translateX(-50%)",
+              fontSize: "24px",
+            }}
+          >
+            &nbsp;&nbsp;Please make sure to update any&nbsp;
+            <RoughNotation
+              type="highlight"
+              color="yellow"
+              animationDelay={15000}
+              animationDuration={1500}
+              iterations={1}
+            >
+              bots
+            </RoughNotation>
+            ,
+            <RoughNotation
+              type="highlight"
+              color="yellow"
+              animationDelay={15500}
+              animationDuration={1500}
+              iterations={1}
+            >
+              &nbsp;software
+            </RoughNotation>
+            , or&nbsp;
+            <RoughNotation
+              type="highlight"
+              color="yellow"
+              animationDelay={16000}
+              animationDuration={1500}
+              iterations={1}
+            >
+              peripherals&nbsp;
+            </RoughNotation>
+            well in
+            <RoughNotation
+              type="circle"
+              color="red"
+              animationDelay={16500}
+              animationDuration={1500}
+              iterations={3}
+            >
+              &nbsp;ADVANCE.
+            </RoughNotation>
+          </div>
+        </RoughNotationGroup>
+        <RoughNotationGroup show={true}>
+          <div
+            style={{
+              position: "absolute",
+              top: 500,
+              left: "50%",
+              transform: "translateX(-50%)",
+              fontSize: "24px",
+            }}
+          >
+            This gives you time to address
+            <RoughNotation
+              type="box"
+              color="red"
+              animationDelay={25000}
+              animationDuration={1500}
+              iterations={4}
+            >
+              &nbsp;any errors or issues&nbsp;
+            </RoughNotation>
+            that may arise due to updates&nbsp;
+            <RoughNotation
+              type="box"
+              color="red"
+              animationDelay={25500}
+              animationDuration={1500}
+              iterations={4}
+            >
+              BEFORE&nbsp;
+            </RoughNotation>
+            you run into issues while you are&nbsp;
+            <RoughNotation
+              type="circle"
+              color="red"
+              animationDelay={26000}
+              animationDuration={1500}
+              iterations={10}
+            >
+              LIVE!&nbsp;&nbsp;
+            </RoughNotation>
+          </div>
+        </RoughNotationGroup>
+        <RoughNotationGroup show={true}>
+          <div
+            style={{
+              position: "absolute",
+              top: 650,
+              left: "50%",
+              transform: "translateX(-50%)",
+              fontSize: "24px",
+            }}
+          >
+            <RoughNotation
+              type="bracket"
+              color="red"
+              animationDelay={30000}
+              animationDuration={1500}
+              brackets={["left", "right"]}
+              strokeWidth={3}
+              padding={[2, 10]}
+            >
+              Unless your niche is a broken stream then&nbsp;
+              <RoughNotation
+                type="highlight"
+                color="yellow"
+                animationDelay={35000}
+                animationDuration={1500}
+                iterations={1}
+              >
+                YOLO!
+              </RoughNotation>
+            </RoughNotation>
+          </div>
+        </RoughNotationGroup>
       </div>
     </main>
   );
